4e4c45b6c1946825c051b955f79ba94ccb7eab92
Navjot Kaur <navjot304@gmail.com>
2017-04-19 17:34:25 -0400

    Index route update

--- COMP308-W2017-MidTerm-300869349/server/routes/index.js
+++ COMP308-W2017-MidTerm-300869349/server/routes/index.js
@@ -9,22 +9,24 @@ Web app name: https://comp308-2017-midterm-300869349.herokuapp.com/
 let express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
-let passport =require('passport');
+let passport = require('passport');
 
-//define user model
-let userModel=require('../models/users');
-let User=userModel.User;
+// define the user model
+let UserModel = require('../models/users');
+let User = UserModel.User; // alias for User Model - User object
 
 // define the book model
 let book = require('../models/books');
 
+
+// create a function to check if the user is authenticated
 function requireAuth(req, res, next) {
+  // check if the user is logged in
   if(!req.isAuthenticated()) {
     return res.redirect('/login');
   }
   next();
 }
-
 
 /* GET home page. wildcard */
 router.get('/', (req, res, next) => {
@@ -32,86 +34,87 @@ router.get('/', (req, res, next) => {
     title: 'Home',
     books: '',
     displayName: req.user ? req.user.displayName : ''
-    
    });
 });
 
 
-/* GET /login - render the login view */
-router.get('/login', (req, res, next) => {
-  // check to see  if the user is not already logged index
+
+// GET /login - render the login view
+router.get('/login', (req, res, next)=>{
+  // check to see if the user is not logged in yet
   if(!req.user) {
     // render the login page
     res.render('auth/login', {
-      title: 'Login',
+      title: "Login",
       books: '',
       messages: req.flash('loginMessage'),
       displayName: req.user ? req.user.displayName : ''
     });
     return;
   } else {
-    return res.redirect('/books'); // redirect to the games list
+    return res.redirect('/books'); // redirect to books list
   }
-});
+}); 
 
-// POST /login - process the login page
+// POST /login - process the login attempt
 router.post('/login', passport.authenticate('local', {
   successRedirect: '/books',
   failureRedirect: '/login',
   failureFlash: 'bad login'
 }));
 
-// GET /register - render the register page
-router.get('/register', (req, res, next) =>{
-  // check if the user is not already logged in
+
+// GET /register - render the registration view
+router.get('/register', (req, res, next)=>{
+   // check to see if the user is not already logged in
   if(!req.user) {
     // render the registration page
-    res.render('auth/register', {
-      title: 'Register',
+      res.render('auth/register', {
+      title: "Register",
       books: '',
       messages: req.flash('registerMessage'),
       displayName: req.user ? req.user.displayName : ''
     });
     return;
-  }
-  else{
-    return res.redirect('/books');
+  } else {
+    return res.redirect('/books'); // redirect to books list
   }
 });
 
-// POST /register - process the registration view
-router.post('/register', (req, res, next) => {
+// POST / register - process the registration submission
+router.post('/register', (req, res, next)=>{
   User.register(
     new User({
-        username: req.body.username,
-        email: req.body.email,
-        displayName: req.body.displayName
-      }),
-      req.body.password,
-      (err) => {
-        if(err) {
-          console.log('Error insterting new user');
-          if(err.name == 'UserExistsError') {
-            req.flash('registerMessage', 'Registration Error: User Already Exists!');
-          }
-          return res.render('auth/register', {
-            title: 'Register',
-            books: '',
-            messages: req.flash('registerMessage'),
-            displayName: req.user ? req.user.displayName : ''
-          });
+      username: req.body.username,
+      email: req.body.email,
+      displayName: req.body.displayName
+    }),
+    req.body.password,
+    (err) => {
+      if(err) {
+        console.log('Error inserting new user');
+        if(err.name == "UserExistsError") {
+          req.flash('registerMessage', 'Registration Error: User Already Exists');
         }
-        // when registration is done successfully
-        return passport.authenticate('local')(req, res, ()=>{
-          res.redirect('/books');
+        return res.render('auth/register', {
+          title: "Register",
+          books: '',
+          messages: req.flash('registerMessage'),
+          displayName: req.user ? req.user.displayName : ''
         });
+      }
+      // if registration is successful
+      return passport.authenticate('local')(req, res, ()=>{
+        res.redirect('/books');
       });
+    });
 });
 
-// GET /logout - logging out the user and then redirect user to the home page
+// GET /logout - process the logout request
 router.get('/logout', (req, res, next)=>{
   req.logout();
-  res.redirect('/'); // redirect to homepage
+  res.redirect('/'); // redirect to the home page
 });
+
 
 module.exports = router;
